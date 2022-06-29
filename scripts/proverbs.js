bb734118fcb76e61636de4b75fb68b05d25bc4fd
BoDing <dingbo8128@163.com>
2022-06-29 09:51:26 +0800

    proverbs

--- scripts/proverbs.js
+++ scripts/proverbs.js
@@ -1,12 +1,14 @@
 import fs from "fs";
 
-const  buf = fs.readFileSync("../docs/02-ot/o20.mdx");
+const buf = fs.readFileSync("../docs/02-ot/20-proverbs.mdx");
 const content = buf.toString();
 const lines = content.split("\n");
 for (let i = 0; i < lines.length; i++) {
-     if (lines[i].startsWith("##")) {
-        lines[i] = '\n' + lines[i] + '\n';
-     }
+    if (lines[i].trim().length === 0) {
+        if (lines[i - 1].trim().endsWith("<br/>") && !lines[i + 1].startsWith("##")) {
+            lines[i] = '<br/>'
+        }
+    }
 }
 
 const newContent = lines.join("\n");
