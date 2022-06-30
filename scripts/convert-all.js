98d003ade46e88bfe19a3308fb03bb44a1b756d6
BoDing <dingbo8128@163.com>
2022-06-30 23:36:50 +0800

    rm all sup

--- scripts/convert-all.js
+++ scripts/convert-all.js
@@ -6,13 +6,13 @@ function convert(buf) {
     const content = buf.toString();
     const lines = content.split("\n");
     for (let i = 1; i < lines.length; i++) {
-        let line = lines[i].trime();
+        let line = lines[i].trim();
         line = line.replaceAll(/<sup>\d+<\/sup>/g, '');
         if (line.startsWith("##")) {
             if (lines[i - 1] != '') {
                 line = '\n' + line;
             }
-            if (lines[i + 1].trime() != '' && !line[i + 1].startsWith('##')) {
+            if (lines[i + 1].trim() != '' && !lines[i + 1].startsWith('##')) {
                 line = '\n' + line + '\n';
             }
         }
